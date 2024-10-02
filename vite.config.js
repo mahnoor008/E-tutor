0a91313422b30d0b2ac397dd10513a80acdbb230
mahnoor008 <mahnoorzulfiqar2000@gmail.com>
2024-10-03 02:48:25 +0500

    Rename dist to docs and update GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/E-tutor/',  // Replace 'your-repository-name' with the actual repository name
+  base: '/E-tutor/',
+  build: {
+    outDir: 'docs'  // Output files to the docs folder
+  },
   plugins: [react()],
 })
